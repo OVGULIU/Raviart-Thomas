bd17683d66987ae12157799e8c6ca0ca5104e418
Simon Heeg <heeg@mathematik.tu-darmstadt.de>
2015-04-21 18:01:36 +0200

    complete mass matrix and added l-shape problems

--- mass_matrix.m
+++ mass_matrix.m
@@ -1,4 +1,4 @@
-function [ M ] = mass_matrix( elems2edges,B,detB,signs)
+function [ MASS ] = mass_matrix( elems2edges,B,detB,signs)
 %MASS_MATRIX Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -12,6 +12,32 @@ ip = [1.66666666666666e-01  6.66666666666667e-01;
 
 weight = ones(3,1).*1.66666666666667e-01;
 
+% reference basis function curl values on integration points,
+% and number of basis functions
+[val,~] = basis(ip);
+
+% calculate all local stiffness matrices simultaneously
+% (without calculating symmetric entries twice)
+MASS = zeros(3,3,numberOfElems);
+for i=1:3
+    for m=1:3
+        for k=m:3
+            MASS(m,k,:) = squeeze(MASS(m,k,:))' + ...
+                          weight(i) .* detB'.^(-1) .* ...
+                          sum( squeeze( astam(signs(:,m), amsv(B, val(i,:,m))) ) ...
+                               .* ...
+                               squeeze( astam(signs(:,k), amsv(B, val(i,:,k))) ) ...
+                             );
+        end
+    end
+end
+
+% copy symmetric entries of the local matrices
+MASS = copy_triu(MASS);
+
+Y = reshape(repmat(elems2edges',3,1),3,3,numberOfElems);     % y-indexes
+X = permute(Y,[2 1 3]);                                       % x-indexes
+MASS = sparse(X(:),Y(:),MASS(:));                             % mass matrix
 
 end
 
--- run.m
+++ run.m
@@ -1,11 +1,18 @@
-load coordinates.dat
-load elements.dat
+addpath('./library_vectorization')
+
+%problemset = 'twotriangles';
+problemset = 'l-shape';
+
+path = ['./problem_settings/'  problemset  '/'];
+coordinates = load([path  'coordinates.dat']);
+elements = load([path  'elements.dat']);
+
 
 % nodes2coordinates = coordinates
 % elems2nodes        = elements
 
 % print mesh
-figure(1); show_mesh(elements,coordinates); axis([0 8 0 6]); title('mesh');
+figure(1); show_mesh(elements,coordinates); title('mesh');
 
 % calculate lookup matrices:
 %   elems2edges: i-th row gives the edges of the i-th triangle
@@ -14,11 +21,14 @@ figure(1); show_mesh(elements,coordinates); axis([0 8 0 6]); title('mesh');
 
 % calculate the affine transformation between each triangle and the
 % reference triangle
-[B,b,detB] = transformations(coordinates,elements);
+[B_F,b_F,detB_F] = transformations(coordinates,elements);
 
 % since 
 signs = get_signs(elements);
 
 % assemble mass matrix
-M = mass_matrix(elems2edges,B,detB,signs);
+A = mass_matrix(elems2edges,B_F,detB_F,signs);
+
+% assemble stiffness matrix
+
 
