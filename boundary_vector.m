5293764147219e2a198b6ab37faa1a1d7b68b8ff
Simon Heeg <heeg.simon@gmail.com>
2015-04-22 11:55:50 +0200

    struggeling with boundary vector

--- boundary_vector.m
+++ boundary_vector.m
@@ -1,6 +1,10 @@
-function [ output_args ] = boundary_vector( input_args )
+function [ VECTOR ] = boundary_vector( coordinates, elements, u_0, g, epsilon)
 %BOUNDARY_VECTOR Summary of this function goes here
 %   Detailed explanation goes here
+
+% (b_R)_j = \int_{boundary} (u_0 + epsilon * g)(basis_j * outer unit
+% normal) ds
+
 
 
 end
